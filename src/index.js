7198f9813851083328e17922e5676de02c767ab8
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-04-12 12:50:20 +0100

    added html through JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Sat", "Sun", "Mon", "Tue", "Wed"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-icon">☀️</div>
+          <div class="weather-forecast-temperatures">
+            <span class="max-temp">18° </span><span class="min-temp">12°</span>
+          </div>
+          </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+displayForecast();
